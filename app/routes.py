d7a41d8e2f56b2d2e5e865a473874612376207eb
RadinMan <Radni0406@gmail.com>
2024-04-20 23:38:24 +0800

    Created mock user and fix routing to the user's home page (home.html file). Currently user is directed to home page only

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from app import app
+from . import app
+from flask import render_template
 # This file is responsible for the routing between the different flask python files and front end html files
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Radin'}
+    return render_template('home.html', title='Home', user=user)
