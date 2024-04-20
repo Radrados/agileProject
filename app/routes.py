20330df2d286e5261384bd3c10a5f3ccb0ef8390
RadinMan <Radni0406@gmail.com>
2024-04-21 00:42:23 +0800

    Added routes to the other webpages such as landing, login, register and logout. The logout route is the same as the landing page because all user's journey begin from here.

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,33 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
+    # reference: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
+    # this is only to have something in the webserver to see when webpage is rendered
     user = {'username': 'Radin'}
-    return render_template('home.html', title='Home', user=user)
+    posts = [{
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'},
+    {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+    }]
+    return render_template('home.html', title='Home', user=user, posts=posts)
+
+
+# Plain landing page
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
+# Require authentication
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/logout')
+def logout():
+    return render_template('landing.html')
