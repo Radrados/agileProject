6911699145f94e983c747524f46f680f07b369ea
RadinMan <Radni0406@gmail.com>
2024-04-21 19:16:38 +0800

    Added app.config secret key to encrypt the cookies and session data

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 # This python file creates the flask app
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'agile-project'  # encrypt the cookies and session data of our 
+
 
 from app import routes
 
