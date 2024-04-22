d6f1a9b1724ab37568aa8e28ec8981295d266f68
RadinMan <Radni0406@gmail.com>
2024-04-22 11:35:57 +0800

    Added database and migrate engine imported from flask_sqlalchemy and flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # This python file creates the flask app
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)  # db object representing the database
+Migrate = Migrate(app, db) # migrate object to represent migration engine
+
 
 
 from app import routes
