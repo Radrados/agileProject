42eb8e9168934b14036726b0d677329a6633a07e
RadinMan <Radni0406@gmail.com>
2024-05-13 16:01:16 +0800

    Imported the Moment.js flask interface and initialised it with the app.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 # This python file creates the flask app
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)  # db object representing the database
 migrate = Migrate(app, db)  # migrate object to represent migration engine
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
 
 from app import routes, models
 
