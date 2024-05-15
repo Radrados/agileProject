bb6033ca741da81fdd11dcb255286ac2a17a99a3
RadinMan <Radni0406@gmail.com>
2024-05-15 10:26:31 +0800

    Added Search route and function, user enters a phrase from the title or body of post and gets results.

--- app/routes.py
+++ app/routes.py
@@ -135,3 +135,26 @@ def user(username):
     posts = Post.query.filter_by(user_id = user.id)
     posts = posts.order_by(Post.timestamp.desc()).all()
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/search', methods=['POST'])
+def search(): 
+    search_post = request.form.get('search-post')
+    if not search_post:
+        flash('You must enter textphrase to search for in title', category='error')
+        return redirect(url_for('index'))
+    
+    # If the user searchs for i.e. 'Introductory Chugg' but there is only 'Introductory to Chugg', we will not get the result
+    # Split the search_post string into an array of tags and query for each tag
+    search_tags = search_post.split(' ')
+    if not search_tags:
+        flash("No valid tags found", category='error')
+        return redirect(url_for('index'))
+    
+    conditions = [] # creating a list of search conditions
+    for tag in search_tags: # for each tag in the array, search the Post title and body for the tag
+        conditions.append(Post.title.ilike(f'%{tag}%'))
+        conditions.append(Post.body.ilike(f'%{tag}%'))
+        
+    query = sql_al.or_(*conditions) # create the query that will accept the tags either in the title or the body
+    search_results = Post.query.filter(query).all()       
+    return render_template('home.html', posts=search_results)
