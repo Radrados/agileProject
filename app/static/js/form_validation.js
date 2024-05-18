b906ea082afbfc4b6b33e922aefc0e0cce62f76f
radrados <radosmarkovic03@gmail.com>
2024-05-18 22:51:01 +0800

    stylysed and responsive

--- app/static/js/form_validation.js
+++ app/static/js/form_validation.js
@@ -2,7 +2,21 @@ function validatePassword() {
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirm-password").value;
     if (password != confirmPassword) {
-        alert("Passwords do not matches.");
+        var modal = document.getElementById("customModal");
+        var span = document.getElementsByClassName("close")[0];
+
+        modal.style.display = "block"; // Show the modal
+
+        span.onclick = function() {
+            modal.style.display = "none"; // Hide the modal when the close button is clicked
+        };
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none"; // Hide the modal when clicking outside of it
+            }
+        };
+
         return false; // Prevent form submission
     }
     return true; // Allow form submission
